ed6ee6edc114747ab845f344dc30932e19bc42e7
wiz <wizlightdragon@gmail.com>
2019-01-27 22:22:47 +0200

    ttest

--- Gemfile
+++ Gemfile
@@ -8,8 +8,7 @@ end
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.0.6'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
@@ -41,9 +40,15 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'debbie', '~> 2.0.0'
 
+group :production do
+  gem 'pg'
+end
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  # Use sqlite3 as the database for Active Record
+  gem 'sqlite3'
 end
 
 group :development do
--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -2,7 +2,12 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
   include Telegram::Bot::UpdatesController::MessageContext
 
   def start!(*)
-    respond_with :message, text: t('.content')
+    respond_with :message, text: 'What would you like to do?', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Buy/Sell a Commodity', callback_data: 'commodity'}],
+            [{text: 'Check Market news', callback_data: 'news'}],
+        ],
+    }
   end
 
   def help!(*)
@@ -31,32 +36,192 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     else
       save_context :keyboard!
       respond_with :message, text: t('.prompt'), reply_markup: {
-        keyboard: [t('.buttons')],
-        resize_keyboard: true,
-        one_time_keyboard: true,
-        selective: true,
+          keyboard: [t('.buttons')],
+          resize_keyboard: true,
+          one_time_keyboard: true,
+          selective: true,
       }
     end
   end
 
   def inline_keyboard!(*)
-    respond_with :message, text: t('.prompt'), reply_markup: {
-      inline_keyboard: [
-        [
-          {text: t('.alert'), callback_data: 'alert'},
-          {text: t('.no_alert'), callback_data: 'no_alert'},
+    respond_with :message, text: 'What would you like to do?', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Buy/Sell a Commodity', callback_data: 'commodity'}],
+            [{text: 'Check Market news', callback_data: 'news'}],
         ],
-        [{text: t('.repo'), url: 'https://github.com/telegram-bot-rb/telegram-bot'}],
-      ],
     }
   end
 
+  def type_market_update!(*)
+    respond_with :message, text: 'Which type of market update would you like?', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Freight prices', callback_data: 'thx'}],
+            [{text: 'Supply and demand figures', callback_data: 'thx'}],
+            [{text: 'Physical Commodity Prices', callback_data: 'thx'}],
+        ],
+    }
+  end
+
+  def commodity_type!(*)
+    respond_with :message, text: 'Which Commodity would you like to sell?', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Grains', callback_data: 'grains'}],
+            [{text: 'Vegetable Oil', callback_data: 'oil'}],
+        ],
+    }
+  end
+
+  def grains!(*)
+    respond_with :message, text: 'Choose the type of grain', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Wheat', callback_data: 'wheat'}],
+            [{text: 'Corn', callback_data: 'wheat'}],
+            [{text: 'Barley', callback_data: 'wheat'}],
+        ],
+    }
+  end
+
+  def oil!(*)
+    respond_with :message, text: 'Choose the type of grain', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Sunflower Oil', callback_data: 'sunflower_oil'}],
+            [{text: 'Rapeseed Oil', callback_data: 'rapeseed_oil'}],
+            [{text: 'Soyabean oilarley', callback_data: 'soyabean_oilarley'}],
+        ],
+    }
+  end
+
+  def sunflower_oil!(*)
+    respond_with :message, text: 'Choose the type of oil', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Crude Sunflower Oil', callback_data: 'bulk_or_bottled'}],
+            [{text: 'Refined Sunflower Oil', callback_data: 'bulk_or_bottled'}],
+        ],
+    }
+  end
+
+  def rapeseed_oil!(*)
+    respond_with :message, text: 'Choose the type of oil', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Crude Rapeseed Oil', callback_data: 'bulk_or_bottled'}],
+            [{text: 'Refined Rapeseed Oil', callback_data: 'bulk_or_bottled'}],
+        ],
+    }
+  end
+
+  def soyabean_oilarley!(*)
+    respond_with :message, text: 'Choose the type of oil', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Crude Soyabean Oil', callback_data: 'bulk_or_bottled'}],
+            [{text: 'Refined Soyabean Oil', callback_data: 'bulk_or_bottled'}],
+        ],
+    }
+  end
+
+  def bulk_or_bottled!(*)
+    respond_with :message, text: 'Choose packing materials', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Bulk', callback_data: 'shipment_period'}],
+            [{text: 'Bottled', callback_data: 'shipment_period'}],
+        ],
+    }
+  end
+
+  def wheat!(*)
+    respond_with :message, text: 'Choose the type of grain', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Feed wheat', callback_data: 'shipment_period'}],
+            [{text: 'Milling wheat', callback_data: 'shipment_period'}],
+        ],
+    }
+  end
+
+  def shipment_period!(*)
+    respond_with :message, text: 'Which shipment period can you sell your commodity?', reply_markup: {
+        inline_keyboard: [
+            [{text: 'first half of month', callback_data: 'thx'}],
+            [{text: 'second half of month', callback_data: 'thx'}],
+            [{text: 'whole month', callback_data: 'incoterms'}],
+        ],
+    }
+  end
+
+  def incoterms!(*)
+    respond_with :message, text: 'Which incoterms can you sell on?', reply_markup: {
+        inline_keyboard: [
+            [
+                {text: 'CIF', callback_data: 'set_incoterms'},
+                {text: 'FOB', callback_data: 'set_incoterms'},
+                {text: 'FCA', callback_data: 'set_incoterms'},
+                {text: 'CPT', callback_data: 'set_incoterms'},
+                {text: 'CFR', callback_data: 'set_incoterms'},
+                {text: 'DAP', callback_data: 'set_incoterms'},
+            ],
+        ],
+    }
+  end
+
+  def payment_terms!(*)
+    respond_with :message, text: 'What payment terms can you sell on?', reply_markup: {
+        inline_keyboard: [
+            [{text: 'Cash against documents', callback_data: 'set_payment_terms'}],
+            [{text: 'Letter of credit', callback_data: 'set_payment_terms'}],
+            [{text: 'Partial prepayment', callback_data: 'set_payment_terms'}],
+            [{text: 'Full prepayment', callback_data: 'set_payment_terms'}],
+        ],
+    }
+  end
+
+  def request_valid!(*)
+    respond_with :message, text: 'Until when is this request valid? Please send a command "/expire_date" with message in the format MM/DD/YYYY'
+  end
+
+  def expire_date!(date)
+    respond_with :message, text: "#{from['first_name']}, thx. Please put your 20$ deposit while we search for a suitable buyer. Incase we do not find a suitable buyer before the validity period expires, we guarantee to return the deposit"
+  end
+
   def callback_query(data)
-    if data == 'alert'
-      answer_callback_query t('.alert'), show_alert: true
+    case data
+    when 'commodity'
+      commodity_type!
+    when 'news'
+      type_market_update!
+    when 'thx'
+      respond_with :message, text: "#{from['first_name']}, thx"
+    when 'grains'
+      grains!
+    when 'vegetable_oil'
+      vegetable_oil!
+    when 'wheat'
+      wheat!
+    when 'shipment_period'
+      shipment_period!
+    when 'incoterms'
+      incoterms!
+    when 'set_incoterms'
+      payment_terms!
+    when 'set_payment_terms'
+      request_valid!
+    when 'oil'
+      oil!
+    when 'sunflower_oil'
+      sunflower_oil!
+    when 'rapeseed_oil'
+      rapeseed_oil!
+    when 'soyabean_oilarley'
+      soyabean_oilarley!
+    when 'bulk_or_bottled'
+      bulk_or_bottled!
     else
-      answer_callback_query t('.no_alert')
+      puts "it was something else"
     end
+
+    # if data == 'products'
+    #   answer_callback_query t('.alert'), show_alert: true
+    # else
+    #   answer_callback_query t('.no_alert')
+    # end
   end
 
   def message(message)
@@ -69,13 +234,13 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     t_content = t('.content')
     results = Array.new(5) do |i|
       {
-        type: :article,
-        title: "#{query}-#{i}",
-        id: "#{query}-#{i}",
-        description: "#{t_description} #{i}",
-        input_message_content: {
-          message_text: "#{t_content} #{i}",
-        },
+          type: :article,
+          title: "#{query}-#{i}",
+          id: "#{query}-#{i}",
+          description: "#{t_description} #{i}",
+          input_message_content: {
+              message_text: "#{t_content} #{i}",
+          },
       }
     end
     answer_inline_query results
@@ -99,7 +264,7 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
   def action_missing(action, *_args)
     if action_type == :command
       respond_with :message,
-        text: t('telegram_webhooks.action_missing.command', command: action_options[:command])
+                   text: t('telegram_webhooks.action_missing.command', command: action_options[:command])
     else
       respond_with :message, text: t('telegram_webhooks.action_missing.feature', action: action)
     end
